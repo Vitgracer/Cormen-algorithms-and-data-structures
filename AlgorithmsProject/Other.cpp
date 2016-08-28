a14635077facf404b7f05c970e877604e7dc7faa
alfredfid <alfredfid@gmail.com>
2016-08-28 14:12:40 +0300

    add description

--- AlgorithmsProject/Other.cpp
+++ AlgorithmsProject/Other.cpp
@@ -2,6 +2,12 @@
 #include <climits>
 #include "Other.h"
 
+// ----------------FIND-MAX-CROSSING-SUBARAAY-------------------------
+// Brief description: we have "low", "mid", "high" positions in 
+// A array. Main goal is to find maximal subarray crossing "mid" point
+// -------------------------------------------------------------------
+// avg = teta(n)
+//---------------------------------------------------------
 std::vector<int> Array::findMaxCrossingSubarray(std::vector<int> A, int low, int mid, int high) {
 	int leftSum = INT_MIN;
 	int maxLeft = high;
