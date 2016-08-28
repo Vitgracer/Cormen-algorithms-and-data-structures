8265696fe07d8c3516f674164de01a9156f45d3f
alfredfid <alfredfid@gmail.com>
2016-08-28 16:23:18 +0300

    add Kadane algorithm with teta(n)

--- src/Other.h
+++ src/Other.h
@@ -7,6 +7,7 @@ private:
 	static std::vector<int> findMaxCrossingSubarray(std::vector<int>& A, int low, int mid, int high);
 	static std::vector<int> findMaxSubarray(std::vector<int>& A, int low, int high);
 	static std::vector<int> findMaxSubarrayBF(std::vector<int>& A);
+	static std::vector<int> findMaxSubarrayKadane(std::vector<int>& A);
 public:
 	friend void launchAllOtherAlgorithms();
 };
