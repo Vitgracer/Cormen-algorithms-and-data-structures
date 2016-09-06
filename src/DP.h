7ca81dacb77458fecd62a8933eda7029fc680f2d
alfredfid <alfredfid@gmail.com>
2016-09-07 00:33:37 +0300

    some comments

--- src/DP.h
+++ src/DP.h
@@ -2,7 +2,10 @@
 
 class DP {
 private:
+	// non-effective recursive algorithm
 	static int cutRod(std::vector<int> p, int n);
+
+	// store calculated values in "r"-array
 	static int memoizedCutRod(std::vector<int> p, int n);
 	static int memoizedCutRodAux(std::vector<int> p, std::vector<int> r, int n);
 public:
