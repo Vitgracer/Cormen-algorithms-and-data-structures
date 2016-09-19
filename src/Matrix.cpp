2aaa7033096f7b7ea074588765e79e6ed70195a3
alfredfid <alfredfid@gmail.com>
2016-09-20 00:53:23 +0300

    bugs fixed + groundtruth test

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -11,7 +11,10 @@
 std::pair<std::vector<std::vector<double>>, 
 std::vector<std::vector<double>>> Matrix::LUdecomposition(std::vector<std::vector<double>> A) {
 	const int n = A.size();
+
 	decltype(A) L(n, std::vector<double>(n, 0));
+	for (int i = 0; i < n; i++) L[i][i] = 1;
+
 	decltype(A) U(n, std::vector<double>(n, 0));
 
 	for (int k = 0; k < n; k++) {
@@ -31,11 +34,34 @@ std::vector<std::vector<double>>> Matrix::LUdecomposition(std::vector<std::vecto
 
 	return std::make_pair(L, U);
 }
-
+// --------------------------- SOLVE-LU (p 857) ---------------------------------
+// Brief description: A = LU, Ax = b, LUx = b. Ux = y => Ly = b => find y, find x
+//-------------------------------------------------------------------------------
+// avg = teta (n ^ 2)
+//----------------------------------------------------------------------------
 std::vector<double> Matrix::solveLowerUpperMatrices(std::vector<std::vector<double>> L,
 													std::vector<std::vector<double>> U,
 													std::vector<double> b) {
-	
+	const int n = L.size();
+	std::vector<double> x(n, 0);
+	std::vector<double> y(n, 0);
+
+	for (int i = 0; i < n; i++) {
+		double sum = 0;
+		for (int j = 0; j <= i - 1; j++) {
+			sum += L[i][j] * y[j];
+		}
+		y[i] = b[i] - sum;
+	}
+
+	for (int i = n - 1; i >= 0; i--) {
+		double sum = 0;
+		for (int j = i + 1; j < n; j++) {
+			sum += U[i][j] * x[j];
+		}
+		x[i] = (y[i] - sum) / U[i][i];
+	}
+	return x;
 }
 
 // --------------------------- SOLVE-LU (p 860) ------------------------------
@@ -56,15 +82,15 @@ std::vector<double> Matrix::solveLU(std::vector<std::vector<double>> A, std::vec
 // -------------------------------------------------------
 void launchAllMatrixAlgorithms() {
 	//-------------------------------------------------
-	const int matrixSize = 5;
-	std::vector<std::vector<double>> A(matrixSize, std::vector<double>(matrixSize, 0));
-	std::vector<double> b(matrixSize, 0);
-	for (int i = 0; i < matrixSize; i++) {
-		b[i] = rand();
-		for (int j = 0; j < matrixSize; j++) {
-			A[i][j] = rand();
-		}
-	}
+	const int matrixSize = 4;
+	std::vector<std::vector<double>> A = 
+	{{2, 3,  1, 5}, 
+	 {6, 13, 5,  19}, 
+	 {2, 19, 10, 23}, 
+	 {4, 10, 11, 31}};
+
+	std::vector<double> b = {1,2,3,4};
+	decltype(b) gt = { 175.0 / 8, -13.0 / 4, 32, -13 };
 
 	//-------------------------------------------------
 	auto start = clock();
@@ -73,7 +99,7 @@ void launchAllMatrixAlgorithms() {
 	std::cout << "LUP-decomposition procedure: " << end << " ms" << std::endl;
 
 	//-------------------------------------------------
-	bool check = true;
+	bool check = (result == gt);
 
 	if (check) std::cout << "All Matrix algorithms are correct" << std::endl << std::endl;
 	else std::cout << "Error!" << std::endl << std::endl;
