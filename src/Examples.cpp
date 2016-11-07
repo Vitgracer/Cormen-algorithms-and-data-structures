dcd24d5996fa6171b582e36d257c882f41264f86
alfredfid <alfredfid@gmail.com>
2016-11-07 19:57:45 +0300

    iosif task was implemented

--- src/Examples.cpp
+++ src/Examples.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Examples.h"
 #include "DataStructure.h"
 
@@ -5,9 +6,27 @@
 // Brief description: task to check advantages of forward lists 
 // --------------------------------------------------------------------
 void Examples::IosifTask(int M, int N) {
-
+	link t = new node(1, 0);
+	t->next = t;
+	
+	link x = t;
+	
+	// create cyclic link 
+	for (int i = 2; i <= N; i++) {
+		x = (x->next = new node(i, t));
+	}
+	
+	while (x != x->next) {
+		for (int i = 1; i < M; i++) {
+			x = x->next;
+		}
+		x->next = x->next->next;
+	}
+	std::cout << x->item;
 }
 
 void launchAllExamples() {
+	std::cout << "Iosif task: " << std::endl;
 	Examples::IosifTask(8, 9);
+	std::cout << "All data structures examples were launched!" << std::endl << std::endl;
 }
