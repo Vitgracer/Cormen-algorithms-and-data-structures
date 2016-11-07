a350a8d4c0541386edee9256f0fd04f0ac8cebc6
alfredfid <alfredfid@gmail.com>
2016-11-07 19:21:35 +0300

    add files for data structures

---
+++ src/DataStructure.h
@@ -0,0 +1,8 @@
+#include <vector>
+
+class Substring {
+private:
+	static int naiveStringMatcher(std::vector<int>& T, std::vector<int>& P);
+public:
+	friend void launchAllSubstringAlgorithms();
+};
