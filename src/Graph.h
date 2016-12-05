68ab619c57182a68c79dcad0ff6ab812808dfdfa
alfredfid <alfredfid@gmail.com>
2016-12-05 19:36:41 +0300

    add graph interface

--- src/Graph.h
+++ src/Graph.h
@@ -9,3 +9,24 @@ struct Edge {
 	, w(wIn) 
 	{}
 };
+
+class GRAPH {
+private:
+public:
+	GRAPH(int, bool);
+	~GRAPH();
+	int V() const;
+	int E() const;
+	bool directed() const;
+	int insert(Edge);
+	int remove(Edge);
+	bool edge(int, int);
+
+	class adjIteartor {
+	public:
+		adjIteartor(const GRAPH&, int);
+		int beg();
+		int nxt();
+		bool end();
+	};
+};
