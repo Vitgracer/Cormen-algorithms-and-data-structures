522a6587095b105d3b61fa1e617a0d7095247f0a
alfredfid <alfredfid@gmail.com>
2016-12-07 19:32:52 +0300

    implement graph class based on adjacency matrix

--- src/Graph.h
+++ src/Graph.h
@@ -76,3 +76,47 @@ public:
 	int count();
 	bool connect(int, int);
 };
+
+class DenseGraph {
+private:
+	int Vcnt;
+	int Ecnt;
+	bool digraph;
+	std::vector<std::vector<bool>> adj;
+public:
+	DenseGraph(int V, bool digraphIn = false)
+		: adj(V)
+		, Vcnt(V)
+		, Ecnt(0)
+		, digraph(digraphIn) {
+		for (int i = 0; i < V; i++) {
+			adj[i].assign(V, false);
+		}
+	}
+	int V() const { return Vcnt; }
+	int E() const { return Ecnt; }
+	int directed() const { return digraph; }
+	
+	void insert(Edge e) {
+		int v = e.v;
+		int w = e.w;
+		if (adj[v][w] == false) Ecnt++;
+		adj[v][w] = true;
+		if (!digraph) adj[w][v] = true;
+	}
+
+	void remove(Edge e) {
+		int v = e.v;
+		int w = e.w;
+		if (adj[v][w] == true) Ecnt--;
+		adj[v][w] = false;
+		if (!digraph) adj[w][v] = false;
+	}
+
+	bool edge(int v, int w) const {
+		return adj[v][w];
+	}
+
+	class adjIterator;
+	friend class adjIterator;
+};
